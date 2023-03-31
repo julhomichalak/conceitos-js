7c59b2cf6d7e0456f349e36866881073ffb5e070
Julho Michalak <julhomichalak@gmail.com>
2023-03-31 10:09:35 -0300

    primeiro modulo

---
+++ comentarios.js
@@ -0,0 +1,14 @@
+console.log("Hello world") //alguma coisa
+
+//console.log("Hello world!")
+
+/*
+
+Comentario em bloco
+
+
+console.log("Comentarios em linha")
+console.log("Comentarios em linha")
+console.log("Comentarios em linha")
+*/
+
