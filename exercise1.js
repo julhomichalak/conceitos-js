aea3ba7693c0a327a6efc34fcd38c13ca1dda67f
Julho Michalak <julhomichalak@gmail.com>
2023-06-24 11:46:29 -0300

    update

---
+++ exercise1.js
@@ -0,0 +1,25 @@
+// exercicio 1
+//let weight = 100;
+
+// exercicio 2
+console.log(typeof weight);
+
+//exercicio 3
+let name = "Julho";
+let age = 20;
+let starts = 4.9;
+let isSubscribed = true;
+
+// // exercicio 4
+// let student = {}
+// console.log(typeof student)
+
+// exercicio 5
+let student = {
+  name: "Julho",
+  age: 20,
+  weight: 89.9,
+  isSubscribed: true,
+};
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`);
+
